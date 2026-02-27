9b70594398dc4066c69b383877607f1b43eb28b3
Kaung Htet Myat <programmingrelated1999@gmail.com>
2026-02-27 09:45:44 +0000

    Descriptive Analysis - Average Post Length

--- main.py
+++ main.py
@@ -26,6 +26,11 @@ def get_unique_headlines(df):
 def clean_text(text_column):
     return
 
+# Get Average Post Length
+def get_average_post_len(df):
+    post_len_list = df.post.str.len()
+    return post_len_list.sum()/len(df.post)
+
 # Main Function
 def main():
     if __name__ == "__main__":
@@ -35,6 +40,7 @@ def main():
         real_posts_count, fake_posts_count = get_num_real_and_fake_posts(post_df)
         real_fake_ratio = round(fake_posts_count / real_posts_count, 2)
         unique_headlines = get_unique_headlines(post_df)
+        average_post_len = get_average_post_len(post_df)
         print("DATASET ANALYSIS")
         print("===========================================")
         print("Number Of Records: ", shape[0])
@@ -43,5 +49,7 @@ def main():
         print("Fake Posts: ", fake_posts_count)
         print("Real To Fake Ratio: 1 :", real_fake_ratio)
         print("Unique Headlines Count: ", len(unique_headlines))
+        print(f"Average Post Length: {average_post_len:.2f}")
 
+# Call Main Function
 main()
