cb66339b5161633875b3b86fec7b6380c9e77b6d
Kaung Htet Myat <programmingrelated1999@gmail.com>
2026-03-12 20:43:43 +0000

    Finish Bow And TFIDF Comparison For N Topics

--- part2.py
+++ part2.py
@@ -4,23 +4,72 @@ import numpy as np
 import joblib
 import matplotlib.pyplot as plt
 import numpy as np
-
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import TfidfVectorizer
 from wordcloud import WordCloud
+from sklearn.model_selection import train_test_split
 
-n_topics = [5,6,7,8,9,10,15,20]
-bow_lda_perplexity = []
-bow_models = []
+# Declare Global Variables
+n_topics = [3,5,7,10,15]
 
+# Save Wordcloud Images
+def save_wordcloud(model, vocabulary):
+    for index, topic in enumerate(model.components_):
+        arr = np.array(topic)
+        highest_indexes = arr.argsort()[-10:][::-1]
+        topics_words = [vocabulary[i] for i in highest_indexes]
+        text = ' '.join([str(i) for i in topics_words]) 
+        wordcloud = WordCloud().generate(text)
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.savefig(f'lda/wordcloud/topic{index}.png')
+        plt.close()
+
+# Plot And Save Bow Experiment With Different Perplexities For LDA
+def plot_bow(bow_lda_perplexities):
+    plt.figure(figsize=(8, 5))
+    plt.plot(n_topics, bow_lda_perplexities, 'o-')
+    plt.xlabel('Number of Topics (K)')
+    plt.ylabel('Perplexity')
+    plt.title('LDA: Perplexity vs Number of Topics')
+    plt.tight_layout()
+    plt.savefig('lda/perplexity_vs_k.png')
+    plt.show()
+
+# Plot And Save Comparison Of Bow And TFIDF With Different Perplexities For LDA
+def plot_comparison(bow_lda_perplexities, tfidf_lda_perplexities):
+    plt.figure(figsize=(8, 5))
+    plt.plot(n_topics, bow_lda_perplexities, 'o-', label='BoW')
+    plt.plot(n_topics, tfidf_lda_perplexities, 's-', label='TF-IDF')
+    plt.xlabel('Number of Topics (K)')
+    plt.ylabel('Perplexity')
+    plt.title('BoW vs TF-IDF: Perplexity Comparison')
+    plt.legend()
+    plt.xticks(n_topics)
+    plt.tight_layout()
+    plt.savefig('lda/bow_vs_tfidf_perplexity.png')
+    plt.show()
+
+def train_lda(corpus_bow):
+    models = []
+    perplexities = []
+    for n in n_topics:
+        lda = LatentDirichletAllocation(n_components=n, random_state=0, max_iter=5, learning_method='batch', verbose = True)
+        lda.fit_transform(corpus_bow)
+        models.append(lda)
+        perplexity = lda.perplexity(corpus_bow)
+        perplexities.append(perplexity)
+    return models, perplexities
+
+# Main Function
 def main():
-    # Read Preprocessed CSV Data
+    # Read Preprocessed CSV Data - Drop Any NA values
     post_df = pd.read_csv("social-media-release-tokenized.csv")
     post_df = post_df[post_df['tokens_joined'].notna()]
-    post_df = post_df[:2000]
+    train_df, test_df = train_test_split(post_df, test_size=0.2, random_state=42, stratify=post_df['class_label'])
 
     # Build A Corpus And Vectorizer, Then Vectorizer Build Bag of Words And Vocabulary
-    posts = post_df['tokens_joined'].tolist()
+    posts = train_df['tokens_joined'].tolist()
     vectorizer = CountVectorizer(max_features = 2000)
     posts_bow = vectorizer.fit_transform(posts)
     vocabulary = vectorizer.get_feature_names_out()
@@ -29,37 +78,22 @@ def main():
 
     # LDA
     print("Start of LDA")
-    for n in n_topics:
-        lda = LatentDirichletAllocation(n_components=n, random_state=0, max_iter=5, verbose = True)
-        docs = lda.fit_transform(posts_bow)
-        topics_words = []
-        for topic in lda.components_:
-            arr = np.array(topic)
-            highest_indexes = arr.argsort()[-10:][::-1]
-            topics_words.append([vocabulary[i] for i in highest_indexes])
-            # text_for_wordcloud = ' '.join([str(element) for element in topics_words ]) 
-            # wordcloud = WordCloud().generate(text_for_wordcloud)
-            # plt.imshow(wordcloud, interpolation='bilinear')
-            # plt.axis("off")
-            # plt.show()
-        bow_models.append(lda)
-        perplexity = lda.perplexity(posts_bow)
-        bow_lda_perplexity.append(perplexity)
-        print("Perplexity: ", perplexity)
+    bow_models, bow_lda_perplexities = train_lda(posts_bow)
 
-    highest_index = np.argmin(bow_lda_perplexity)
+    highest_index = np.argmin(bow_lda_perplexities)
     highest_bow_model = bow_models[highest_index]
+    save_wordcloud(highest_bow_model, vocabulary)
+    
     joblib.dump(highest_bow_model, 'lda/best_lda_model.pkl')
     joblib.dump(vectorizer, 'lda/bow_vectorizer.pkl')
 
-    plt.figure(figsize=(8, 5))
-    plt.plot(n_topics, bow_lda_perplexity, 'o-')
-    plt.xlabel('Number of Topics (K)')
-    plt.ylabel('Perplexity')
-    plt.title('LDA: Perplexity vs Number of Topics')
-    plt.tight_layout()
-    plt.savefig('lda/perplexity_vs_k.png')
-    plt.show()
+    plot_bow(bow_lda_perplexities)
+
+    tfidf_vec = TfidfVectorizer(max_features=5000)
+    posts_tfidf = tfidf_vec.fit_transform(posts)
+    tfidf_models, tfidf_lda_perplexities = train_lda(posts_tfidf)
+
+    plot_comparison(bow_lda_perplexities, tfidf_lda_perplexities)
 
 # Main
 if __name__ == "__main__":
